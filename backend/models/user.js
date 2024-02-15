53a2d37fdfddc9af3b7a9da1c2071cad0b690795
TusharPDev <tusharpatil0622@gmail.com>
2024-02-15 23:43:17 +0530

    new commit for signin/signup functionality

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,10 +4,12 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
+        unique:true,
         required:true,
     },
     username:{
         type:String,
+        dunique:true,
         required:true,
     },
     password:{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 //now we are creating Api's for user sign up functionality
 const router = require("express").Router();
 const User = require("../models/user")
+const bcrypt = require("bcryptjs")
+
 
 //here we are creating signup functionality
 router.post("/register", async (req,res)=>{
@@ -10,9 +12,9 @@ router.post("/register", async (req,res)=>{
 
         //here we are telling database that whenever someone wll come to the signup page
         // will be a new user always.
-        
+        const hashPassword = bcrypt.hashSync(password)
         //and we send data like this "{email,username,password}" cause mongo db works with obj's
-        const user = new User({email,username,password}); 
+        const user = new User({email,username,password : hashPassword}); 
 
         await user.save().then(() => res.status(200).json({ user: user }));
     } catch (error) {
@@ -20,8 +22,25 @@ router.post("/register", async (req,res)=>{
     }
 });
 
-router.post("/login", async (req,res)=>{
 
+//here we are creating signin functionality
+
+router.post("/login", async (req,res)=>{
+    try {
+        const user = await User.findOne({email:req.body.email})
+        if(!user){
+            res.status(400).json({message:"Please Sign Up first!"})
+        }
+
+        const isPassword = bcrypt.compareSync(req.body.password, user.password)
+        if(!isPassword){
+            res.status(400).json({message:"Incorrect password!"}) 
+        }
+
+        const {password,...others} = user._doc;
+    } catch (error) {
+        res.status(400).json({message:"User Already registered"});
+    }
 })
 
 module.exports = router;
