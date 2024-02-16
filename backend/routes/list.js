77d58aa1a2a7a31d96803490f9f832584f1823a7
TusharPDev <tusharpatil0622@gmail.com>
2024-02-17 00:08:34 +0530

    update todo commit

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -1,27 +1,44 @@
-const router =require("express").Router();
-const User = require("../models/user")
-const List  = require("../models/list")
+const router = require("express").Router();
+const User = require("../models/user");
+const List = require("../models/list");
+const list = require("../models/list");
 
-router.post("/addTask",async (req,res)=>{
-    try {
-        const {title,body,email} = req.body;
-    const existingUser = await User.findOne({email});
-    if(existingUser){
-        const list = new List({title,body,user:existingUser})
-        await list.save().then(()=>res.status(200).json({list}));
-        existingUser.list.push(list);
-        existingUser.save()
-    }else {
-        res.status(404).json({ error: "User not found" });
+
+//create
+router.post("/addTask", async (req, res) => {
+  try {
+    const { title, body, email } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const list = new List({ title, body, user: existingUser });
+      await list.save().then(() => res.status(200).json({ list }));
+      existingUser.list.push(list);
+      existingUser.save();
+    } else {
+      res.status(404).json({ error: "User not found" });
     }
-} catch (error) {
+  } catch (error) {
     console.error("Error adding task:", error);
     res.status(500).json({ error: "Internal Server Error" });
-}
-})
+  }
+});
 
+//update
 
+router.put("/updateTask/:id", async (req, res) => {
+    try {
+      const { title, body, email } = req.body;
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+       const list = await List.findByIdAndUpdate(req.params.id,{title,body});
+        list.save().then(()=>res.status(200).json({message: "Task Updated"}))
+      } else {
+        res.status(404).json({ error: "User not found" });
+      }
+    } catch (error) {
+      console.error("Error adding task:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 
-
-
-module.exports=router;
+module.exports = router;
