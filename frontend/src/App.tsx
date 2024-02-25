4442fdc5d485700ac514a7bd4f64d780fe3d7254
TusharPDev <tusharpatil0622@gmail.com>
2024-02-25 23:18:57 +0530

    ROutes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/navbar';
-import useLocalStorage from 'use-local-storage';
-import { useAtom } from 'jotai';
-import { themeToggleAtom } from './jotai-store/atoms/navbarAtom';
-import Home from './Components/Home/home';
-import Footer from './Components/footer/footer';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/navbar";
+import useLocalStorage from "use-local-storage";
+import { useAtom } from "jotai";
+import { themeToggleAtom } from "./jotai-store/atoms/navbarAtom";
+import Home from "./Components/Home/home";
+import Footer from "./Components/footer/footer";
 
 function App() {
-  const [theme,setTheme] = useAtom(themeToggleAtom)
-
+  const [theme, setTheme] = useAtom(themeToggleAtom);
+  const[mode,setMode]=useLocalStorage("mode",false)
+  
+  const handleToggleTheme = () => {
+    setMode(!mode);
+    setTheme({ ...theme, isDark: !mode });
+  };
   return (
-    <div style={{backgroundColor: theme.isDark ? "#272829" : "#D8D9DA"}} className="App">
-     <Navbar/>
-     <div className='main'>
-      <Home/>
-     </div>
-     <Footer/>
+    <div
+      onClick={handleToggleTheme}
+      style={{ backgroundColor: theme.isDark ? "#272829" : "#D8D9DA" }}
+      className="App"
+    >
+      <Navbar />
+      <Home />
+      <Footer />
     </div>
   );
 }
--- frontend/src/Components/Home/home.tsx
+++ frontend/src/Components/Home/home.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import "./home.css";
 const Home = () => {
   return (
-    <div style={{textAlign:"center",fontWeight:800,padding:"10px 30px",color:"#61677A"}}>
-      <h1>Welcome aboard! Let's navigate your tasks together on this journey.</h1>
+    <div
+      className="homeMain"
+      style={{
+        textAlign: "center",
+        fontWeight: 800,
+        // padding: "10px 30px",
+        color: "#61677A",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center"
+      }}
+    >
+      <h1>
+        Welcome aboard! Let's navigate your tasks together on this journey.
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
